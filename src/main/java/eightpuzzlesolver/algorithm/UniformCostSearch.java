77f9d0c32929a287c0b4ffcbbc9aea4fd1a0a6da
Luiz Berto <diasbertoluiz@gmail.com>
2022-05-15 11:15:46 -0300

    optimize uniform cost search

--- src/main/java/eightpuzzlesolver/algorithm/UniformCostSearch.java
+++ src/main/java/eightpuzzlesolver/algorithm/UniformCostSearch.java
@@ -3,7 +3,9 @@ package eightpuzzlesolver.algorithm;
 import eightpuzzlesolver.Board;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class UniformCostSearch {
@@ -11,6 +13,8 @@ public class UniformCostSearch {
         var priorityQueue = new PriorityQueue<>(Comparator.comparing(Path::steps));
         priorityQueue.add(new Path(initialState, 0));
 
+        Map<Board, Integer> scoreMap = new HashMap<>();
+        scoreMap.put(initialState, 0);
         var explored = new HashSet<Board>();
         while (!priorityQueue.isEmpty()) {
             var current = priorityQueue.remove();
@@ -19,6 +23,7 @@ public class UniformCostSearch {
                 return new Solution(current.steps());
             }
 
+            scoreMap.remove(current.board());
             explored.add(current.board());
             for (var move : current.board().possibleMoves()) {
                 var child = current.board().move(move);
@@ -26,15 +31,17 @@ public class UniformCostSearch {
                 var instancesOfChildInQueue = priorityQueue
                         .stream()
                         .filter(path -> path.board().equals(child)).toList();
-                var stateAlreadyInQueue = !instancesOfChildInQueue.isEmpty();
+                var stateAlreadyInQueue = scoreMap.get(child) != null;
                 if (!explored.contains(child) && !stateAlreadyInQueue) {
+                    scoreMap.put(child, current.steps() + 1);
                     priorityQueue.add(new Path(child, 1 + current.steps()));
                 } else if (stateAlreadyInQueue) {
                     assert instancesOfChildInQueue.size() == 1;
-                    var instance = instancesOfChildInQueue.get(0);
-                    if (instance.steps() > 1 + current.steps()) {
-                        priorityQueue.remove(instance);
+                    var prevScore = scoreMap.get(child);
+                    if (prevScore > 1 + current.steps()) {
+                        priorityQueue.remove(new Path(child, prevScore));
                         priorityQueue.add(new Path(child, 1 + current.steps()));
+                        scoreMap.put(child, current.steps() + 1);
                     }
                 }
             }
