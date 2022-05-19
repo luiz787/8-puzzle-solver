d3c339ee1fd3d1e96a5d69c38bfb0ace2b85c515
Luiz Berto <diasbertoluiz@gmail.com>
2022-05-18 22:49:16 -0300

    change GreedySearch to be a modified version of A*
    the only difference is the priority queue comparison function:
    for greedy search, it only considers the heuristic

--- src/main/java/eightpuzzlesolver/algorithm/GreedySearch.java
+++ src/main/java/eightpuzzlesolver/algorithm/GreedySearch.java
@@ -3,25 +3,50 @@ package eightpuzzlesolver.algorithm;
 import eightpuzzlesolver.Board;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.function.Function;
 
 public class GreedySearch {
+
     public Solution solve(Board initialState, Function<Board, Integer> heuristicFunction) {
-        var queue = new PriorityQueue<>(Comparator.comparing(Path::board, Comparator.comparing(heuristicFunction)));
+        PriorityQueue<Path> queue = new PriorityQueue<>(Comparator.comparing((path -> heuristicFunction.apply(path.board()))));
         queue.add(new Path(initialState, 0));
 
+        Map<Board, Integer> scoreMap = new HashMap<>();
+        scoreMap.put(initialState, 0);
+        Set<Board> open = new HashSet<>();
+        open.add(initialState);
+
+        Set<Board> closed = new HashSet<>();
         while (!queue.isEmpty()) {
             var current = queue.remove();
             if (current.board().numberOfPiecesOnWrongPlace() == 0) {
                 return new Solution(current.steps());
             }
+            open.remove(current.board());
+            scoreMap.remove(current.board());
+            closed.add(current.board());
+
             for (var move : current.board().possibleMoves()) {
                 var child = current.board().move(move);
-                queue.add(new Path(child, current.steps() + 1));
+                if (!closed.contains(child)) {
+                    if (!open.contains(child)) {
+                        scoreMap.put(child, current.steps() + 1);
+                        queue.add(new Path(child, current.steps() + 1));
+                        open.add(child);
+                    } else if (current.steps() + 1 < scoreMap.get(child)) {
+                        queue.remove(new Path(child, scoreMap.get(child)));
+                        scoreMap.put(child, current.steps() + 1);
+                        queue.add(new Path(child, current.steps() + 1));
+                    }
+                }
             }
         }
-
         throw new IllegalStateException("No solution found");
     }
+
 }
