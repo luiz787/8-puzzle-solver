319e3527785f002bdc82cf793b98a6a825c694af
Luiz Berto <diasbertoluiz@gmail.com>
2022-05-19 18:09:32 -0300

    print path UCS

--- src/main/java/eightpuzzlesolver/algorithm/BestScoreContext.java
+++ src/main/java/eightpuzzlesolver/algorithm/BestScoreContext.java
@@ -5,4 +5,7 @@ import eightpuzzlesolver.Board;
 import java.util.List;
 
 record BestScoreContext(Integer score, List<Board> path) {
+    public BestScoreContext(Path path) {
+        this(path.steps(), path.boards());
+    }
 }
--- src/main/java/eightpuzzlesolver/algorithm/UniformCostSearch.java
+++ src/main/java/eightpuzzlesolver/algorithm/UniformCostSearch.java
@@ -5,6 +5,7 @@ import eightpuzzlesolver.Board;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -13,16 +14,16 @@ public class UniformCostSearch implements Algorithm {
     @Override
     public Solution solve(Board initialState) {
         var priorityQueue = new PriorityQueue<>(Comparator.comparing(Path::steps));
-        priorityQueue.add(new Path(initialState, 0));
+        priorityQueue.add(new Path(List.of(initialState), 0));
 
-        Map<Board, Integer> scoreMap = new HashMap<>();
-        scoreMap.put(initialState, 0);
+        Map<Board, BestScoreContext> scoreMap = new HashMap<>();
+        scoreMap.put(initialState, new BestScoreContext(0, List.of(initialState)));
         var explored = new HashSet<Board>();
         while (!priorityQueue.isEmpty()) {
             var current = priorityQueue.remove();
             if (current.currentBoard().numberOfPiecesOnWrongPlace() == 0) {
                 System.out.println("[UCS] Explored states: " + explored.size());
-                return new Solution(current.steps());
+                return new Solution(current.boards(), current.steps());
             }
 
             scoreMap.remove(current.currentBoard());
@@ -35,15 +36,17 @@ public class UniformCostSearch implements Algorithm {
                         .filter(path -> path.currentBoard().equals(child)).toList();
                 var stateAlreadyInQueue = scoreMap.get(child) != null;
                 if (!explored.contains(child) && !stateAlreadyInQueue) {
-                    scoreMap.put(child, current.steps() + 1);
-                    priorityQueue.add(new Path(child, 1 + current.steps()));
+                    Path newChildPath = current.addBoard(child);
+                    scoreMap.put(child, new BestScoreContext(newChildPath));
+                    priorityQueue.add(newChildPath);
                 } else if (stateAlreadyInQueue) {
                     assert instancesOfChildInQueue.size() == 1;
                     var prevScore = scoreMap.get(child);
-                    if (prevScore > 1 + current.steps()) {
-                        priorityQueue.remove(new Path(child, prevScore));
-                        priorityQueue.add(new Path(child, 1 + current.steps()));
-                        scoreMap.put(child, current.steps() + 1);
+                    if (prevScore.score() > 1 + current.steps()) {
+                        priorityQueue.remove(new Path(prevScore.path(), prevScore.score()));
+                        Path newChildPath = current.addBoard(child);
+                        priorityQueue.add(newChildPath);
+                        scoreMap.put(child, new BestScoreContext(newChildPath));
                     }
                 }
             }
